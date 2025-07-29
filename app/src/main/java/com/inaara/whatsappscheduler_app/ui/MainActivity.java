970e4d5d4ebd6f096a10998dc5670019b9225808
Inaara Tuan <intuan21@outlook.com>
2025-07-29 17:44:03 -0400

    feat(ui): link UI to backend in main activity - Processed user input and linked to backend - Triggered message scheduling via Scheduler

--- app/src/main/java/com/inaara/whatsappscheduler_app/ui/MainActivity.java
+++ app/src/main/java/com/inaara/whatsappscheduler_app/ui/MainActivity.java
@@ -1,24 +1,136 @@
 package com.inaara.whatsappscheduler_app.ui;
 
 import android.Manifest;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import com.inaara.whatsappscheduler_app.R;
+import com.inaara.whatsappscheduler_app.data.database.AppDatabase;
+import com.inaara.whatsappscheduler_app.data.model.Message;
+import com.inaara.whatsappscheduler_app.scheduler.Scheduler;
+
+import java.util.Calendar;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+
+    // MANAGING FRONTEND
+    private long scheduledTimeMs = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText contactInput = findViewById(R.id.contact_input);
+        EditText messageInput = findViewById(R.id.message_input);
+        EditText reminderOffsetInput = findViewById(R.id.notify_before_input);
+        Button timePickerButton = findViewById(R.id.time_picker);
+        Button scheduleButton = findViewById(R.id.schedule_button);
+
+        // Time Picker logic
+        timePickerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                int yearCurrent = calendar.get(Calendar.YEAR);
+                int monthCurrent = calendar.get(Calendar.MONTH);
+                int dayCurrent = calendar.get(Calendar.DAY_OF_MONTH);
+                int hourCurrent = calendar.get(Calendar.HOUR_OF_DAY);
+                int minCurrent = calendar.get(Calendar.MINUTE);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        MainActivity.this,
+                        //date picker listener = param 2 to instantiate DatePickerDialog
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                calendar.set(Calendar.YEAR, year);
+                                calendar.set(Calendar.MONTH, month);
+                                calendar.set(Calendar.DAY_OF_MONTH, day);
+
+                                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                                        MainActivity.this,
+                                        // time picker listener = param 2 to instantiate TimePickerDialog
+                                        new TimePickerDialog.OnTimeSetListener() {
+                                            @Override
+                                            public void onTimeSet(TimePicker timePicker, int hour, int min) {
+                                                calendar.set(Calendar.HOUR_OF_DAY, hour);
+                                                calendar.set(Calendar.MINUTE, min);
+                                                scheduledTimeMs = calendar.getTimeInMillis();
+                                            }
+                                        },
+                                        // arguments for Time Picker
+                                        hourCurrent,
+                                        minCurrent,
+                                        false
+                                );
+                                timePickerDialog.show();
+                            }
+                        },
+                        // arguments for Date Picker
+                        yearCurrent,
+                        monthCurrent,
+                        dayCurrent
+                );
+                datePickerDialog.show();
+            }
+        });
+
+        // Schedule Button logic
+        scheduleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (areInputsValid(contactInput, messageInput, reminderOffsetInput, scheduledTimeMs)) {
+
+                    // Backend handling + call to scheduler
+                    try {
+                        String contactName = contactInput.getText().toString();
+                        String messageText = messageInput.getText().toString();
+                        int reminderOffset = Integer.parseInt(reminderOffsetInput.getText().toString());
+
+                        Message message = new Message();
+                        message.setContactName(contactName);
+                        message.setText(messageText);
+                        message.setReminderOffset(reminderOffset);
+                        message.setScheduledTimeMs(scheduledTimeMs);
+
+                        AppDatabase db = AppDatabase.getInstance(MainActivity.this);
+
+                        Executors.newSingleThreadExecutor().execute(() -> {
+                            long id = db.messageDao().insert(message);
+                            message.setId((int) id);
+                            Scheduler scheduler = new Scheduler();
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                                scheduler.scheduleMessage(MainActivity.this, message);
+                            }
+                        });
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    runOnUiThread(() ->
+                        Toast.makeText(MainActivity.this, "Message scheduled", Toast.LENGTH_SHORT).show()
+                    );
+                    contactInput.setText("");
+                    messageInput.setText("");
+                    reminderOffsetInput.setText("");
+                }
+            }
+        });
 
         // Check flag to request permission
         if (getIntent().getBooleanExtra("request_permission", false)) {
@@ -26,6 +138,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public boolean areInputsValid(EditText contact, EditText message, EditText notifyBefore, long scheduledTime) {
+        if (!isRequiredFieldValid(contact)) return false;
+        if (!isRequiredFieldValid(message)) return false;
+        if (!isRequiredFieldValid(notifyBefore)) return false;
+
+        if (scheduledTime <= System.currentTimeMillis()) {
+            notifyBefore.setError("Time entered must be in future.");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isRequiredFieldValid(EditText field) {
+        if (field.length() == 0) {
+            field.setError("Required field.");
+            return false;
+        }
+        return true;
+    }
+
+
+    // MANAGING PERMISSIONS
     final int REQUEST_NOTIFICATION_PERMISSION = 1001;
     public void checkNotificationPermission(Context context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
